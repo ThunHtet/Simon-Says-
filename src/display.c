48e0affa81e35b7947de7174bed2f93b75c022c6
Mackenzie Butler <butle275@purdue.edu>
2025-04-10 02:15:41 -0400

    added display file

---
+++ src/display.c
@@ -0,0 +1,88 @@
+#include "stm32f0xx.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int score(int current){
+    return (10*current);
+}
+
+void spi_cmd(unsigned int data) {
+    while(!(SPI1->SR & SPI_SR_TXE)){ //wait till empty
+
+    }
+    SPI1->DR= data; //copy to SPI1 DR
+
+}
+void spi_data(unsigned int data) {
+    spi_cmd(data | 0x200);
+    
+}
+void nano_wait(unsigned int n) {
+    asm(    "        mov r0,%0\n"
+            "repeat: sub r0,#83\n"
+            "        bgt repeat\n" : : "r"(n) : "r0", "cc");
+}
+void spi1_init_oled() {
+    nano_wait(1000000);
+    spi_cmd(0x38); //function set
+    spi_cmd(0x08); //turn off display
+    spi_cmd(0x01); //clear display
+    nano_wait(2000000);
+    spi_cmd(0x06);//entry mode
+    spi_cmd(0x02);//cursor home postion
+    spi_cmd(0x0c); //display on
+    
+}
+
+//first row
+void spi1_display1(const char *string) {
+    spi_cmd(0x02);//cursor home postion
+    while(*string != '\0'){ //for each character
+        spi_data(*string); //call spi_data
+        string ++;
+    }
+    
+}
+
+//second row
+void spi1_display2(const char *string) {
+    spi_cmd(0xc0);//cursor second row
+    while(*string != '\0'){ //for each character
+        spi_data(*string); //call spi_data
+        string ++;
+    }
+}
+
+void init_spi1() {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER &= ~((3<<(15*2))|(3<<(5*2))|(3<<(7*2))); //clear bits 5,7,15
+    GPIOA->MODER |= ((2<<(15*2))|(2<<(5*2))|(2<<(7*2))); //alternate function
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+    SPI1->CR1 &= ~SPI_CR1_SPE; //clear bit
+    SPI1 ->CR1 |= SPI_CR1_BR;
+    SPI1->CR2 = SPI_CR2_DS_3 | SPI_CR2_DS_0;
+    SPI1->CR1 |= SPI_CR1_MSTR;
+    SPI1->CR2 |= SPI_CR2_SSOE;
+    SPI1 -> CR2 |= SPI_CR2_NSSP;
+    SPI1 -> CR2 |= SPI_CR2_TXDMAEN;
+    SPI1 -> CR1 |= SPI_CR1_SPE;
+
+    
+}
+
+
+//main function to use for testing
+/*
+int main(void){
+    internal_clock();
+    init_spi1();
+    spi1_init_oled();
+    spi1_display1("score");
+    int sc = score(10);
+    char scoreString[16];   // adjust buffer size as needed
+    sprintf(scoreString, "%d", sc);
+    spi1_display2(scoreString);
+    
+    }*/
